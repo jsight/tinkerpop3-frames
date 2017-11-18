e01eb8ed17da5bc22f92cb06297900158a74d747
Jess Sightler <jesse.sightler@gmail.com>
2017-11-17 23:51:38 -0500

    Added support for iterator to the constructor

--- src/main/java/com/tinkerpop/frames/structures/FramedVertexIterable.java
+++ src/main/java/com/tinkerpop/frames/structures/FramedVertexIterable.java
@@ -4,7 +4,9 @@ import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import com.tinkerpop.frames.FramedGraph;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
@@ -13,6 +15,14 @@ public class FramedVertexIterable<T> implements Iterable<T> {
     protected final Class<T> kind;
     protected final Iterable<Vertex> iterable;
     protected final FramedGraph<? extends Graph> framedGraph;
+
+    public FramedVertexIterable(final FramedGraph<? extends Graph> framedGraph, final Iterator<Vertex> iterator, final Class<T> kind) {
+        this.framedGraph = framedGraph;
+        List<Vertex> vertices = new ArrayList<>();
+        iterator.forEachRemaining(vertices::add);
+        this.iterable = vertices;
+        this.kind = kind;
+    }
 
     public FramedVertexIterable(final FramedGraph<? extends Graph> framedGraph, final Iterable<Vertex> iterable, final Class<T> kind) {
         this.framedGraph = framedGraph;
